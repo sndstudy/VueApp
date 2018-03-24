77a732b9af4093a330c38ebac9931a2600eab183
sndstudy <sanada2054tech@gmail.com>
2018-03-25 00:09:35 +0900

    コンポーネント 追加

--- main.js
+++ main.js
@@ -25,3 +25,38 @@ var app4 = new Vue({
         todos:[]
     }
 })
+
+var app5 = new Vue({
+    el:'#app5',
+    data:{
+        msg:"Hello World"
+    },
+    methods:{
+        reverse:function(){
+            this.msg = this.msg.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app6',
+    data: {
+      msg: 'Hello Vue!'
+    }
+  })
+
+Vue.component('todo-item', {
+// todo-item コンポーネントはカスタム属性のような "プロパティ" で受け取ります。
+// このプロパティは todo と呼ばれます。
+props: ['todo'],
+template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+el: '#app7',
+data: {
+    groceryList: [
+    { id: 0, text: 'Vegetables' }
+    ]
+}
+})
